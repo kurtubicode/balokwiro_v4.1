cc0845264ec7882e1789b781d81e3aadf7442774
AK Kurtubi <ariahmadkurtubi@gmail.com>
2025-09-09 01:23:51 +0700

    m

--- backend/koneksi.php
+++ backend/koneksi.php
@@ -6,12 +6,7 @@ $username = "root";
 $password = "";
 $database = "123";
 
-// $host = "localhost";
-// $username = "root";
-// $password = "";
-// $database = "123";
 
-// isi nama host, username mysql, dan password mysql anda
 $koneksi = mysqli_connect($host, $username, $password, $database);
 if (!$koneksi) {
     die("Koneksi database gagal: " . mysqli_connect_error());
